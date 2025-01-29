53d9c09e6c383fd0e75879239f933f61dd25c19f
rsanchezgarc <rubensanchezgarc@gmail.com>
2025-01-29 16:22:32 +0100

    Update datapreprocessor.py
    bug in storing

--- projects/simple_mlp/datapreprocessor.py
+++ projects/simple_mlp/datapreprocessor.py
@@ -9,8 +9,8 @@ from sklearn.datasets import fetch_california_housing
 #I am using the housing price prediction dataset, already cleaned and prepared
 X, y  = fetch_california_housing(return_X_y=True)
 
-torch.as_tensor(X, dtype=torch.float32)
-torch.as_tensor(y, dtype=torch.float32)
+X = torch.as_tensor(X, dtype=torch.float32)
+y = torch.as_tensor(y, dtype=torch.float32)
 print(X.shape, y.shape)
 print(y)
 torch.save(X, "data/input_data.pt")
