3b9f3f6b5db0f443f885bb96e4a44951e35d0b60
rsanchezgarc <rubensanchezgarc@gmail.com>
2025-01-29 18:43:47 +0100

    Update datapreprocessor.py

--- projects/simple_mlp/datapreprocessor.py
+++ projects/simple_mlp/datapreprocessor.py
@@ -6,11 +6,16 @@ from sklearn.datasets import fetch_california_housing
 #input normalization
 #removal of outliers
 
-#I am using the housing price prediction dataset, already cleaned and prepared
+#I am using the housing price prediction dataset, already cleaned and prepared (except for scaling)
 X, y  = fetch_california_housing(return_X_y=True)
 
 X = torch.as_tensor(X, dtype=torch.float32)
 y = torch.as_tensor(y, dtype=torch.float32)
+
+#It might be  a good idea to normalize the inputs and perhaps the targets as well, something like
+# X = (X-X.mean(0))/X.std(0)
+# y = (y-y.mean())/y.std()
+#If you normalize the label, don't forget to unnormalize it in the prediction.
 print(X.shape, y.shape)
 print(y)
 torch.save(X, "data/input_data.pt")
